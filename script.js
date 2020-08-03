c18a1d14bee03eab05d500caeaf0a35dc11d22fd
Sasha Lysykh <lysykh.alex@ukr.net>
2020-08-03 10:46:29 +0300

    03.08

---
+++ script.js
@@ -0,0 +1,8 @@
+let inputIn=document.querySelector(".input-in");
+let button=document.querySelector("button");
+button.onclick=function(){
+    console.log("Let's go!");
+    console.log(inputIn.value);
+    let b =inputIn.value;
+    console.log(b*2);
+}
