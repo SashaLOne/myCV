f1ff736f5a78e0bcbf79d8c62ef2a01cfc18d4bd
Sasha Lysykh <lysykh.alex@ukr.net>
2020-08-04 19:22:58 +0300

    новый сайт

--- script.js
+++
@@ -1,8 +0,0 @@
-let inputIn=document.querySelector(".input-in");
-let button=document.querySelector("button");
-button.onclick=function(){
-    console.log("Let's go!");
-    console.log(inputIn.value);
-    let b =inputIn.value;
-    console.log(b*2);
-}
